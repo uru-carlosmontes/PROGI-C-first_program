71c5789f1cb93cb810760cc8bf217fccd74c5a8e
carlosmontes002 <carlosmontes002@gmail.com>
2016-04-09 20:29:08 -0430

    first commit

---
+++ first_program.cpp
@@ -0,0 +1,33 @@
+// Esto es un comentario
+
+// Estas son librerias
+/*
+	Las Librerias tienen la funcion
+	de ayudarnos a crear programas
+	o aplicaciones de forma mas 
+	rapida, es decir, nos ahorran
+	tiempo al momento de escribir
+	nuestro codigo, ya que estas
+	nos proveen funciones que
+	simplifican muchos procesos.
+*/
+
+//Esta libreria nos permite usar 
+//las funciones printf() y scanf()
+#include <stdio.h> 
+//Esta otra nos permite usar getch()
+#include <conio.h>
+
+// 	main es la funcion principal del programa
+//	Al momento de compilarlo el IDE sabe
+//	que debe buscar esta funcion, ya que para el
+//	esta es quien le da toda la funcionalidad al 
+//	programa.
+int main () {
+//	printf() permite mostrar mensajes en la pantalla
+	printf("Mi Primer Programa");
+//	getch() espera a que una tecla sea presionada
+	getch();
+	return 0;
+}
+
